4c64a9e6f9f36ded3fd3a8f3798120366d9c605b
mexmarv <marvin@winesfrombaja.com>
2014-07-01 15:16:12 -0500

    4 security checks
    3 security checks in isValidate() and loadXml() - XXE/XEE

--- com/onelogin/saml/Response.java
+++ com/onelogin/saml/Response.java
@@ -8,8 +8,6 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import javax.xml.XMLConstants;
 import javax.xml.crypto.dsig.XMLSignature;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
@@ -27,102 +25,123 @@ import org.xml.sax.SAXException;
 
 public class Response {
 
-	private Document xmlDoc;
-	private final AccountSettings accountSettings;
-	private final Certificate certificate;
+    private Document xmlDoc;
+    private Integer Assertions;
+    private Element rootElement;
+    private final AccountSettings accountSettings;
+    private final Certificate certificate;
 
-	public Response(AccountSettings accountSettings) throws CertificateException {
-		this.accountSettings = accountSettings;
-		certificate = new Certificate();
-		certificate.loadCertificate(this.accountSettings.getCertificate());
-	}
+    public Response(AccountSettings accountSettings) throws CertificateException {
+        this.accountSettings = accountSettings;
+        certificate = new Certificate();
+        certificate.loadCertificate(this.accountSettings.getCertificate());
+    }
 
-	public void loadXml(String xml) throws ParserConfigurationException, SAXException, IOException {
-		DocumentBuilderFactory fty = DocumentBuilderFactory.newInstance();
-		fty.setNamespaceAware(true);
-		fty.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-		DocumentBuilder builder = fty.newDocumentBuilder();
-		ByteArrayInputStream bais = new ByteArrayInputStream(xml.getBytes());
-		xmlDoc = builder.parse(bais);
-	}
+    public void loadXml(String xml) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory fty = DocumentBuilderFactory.newInstance();
+        fty.setNamespaceAware(true);
+        // XMLConstants with FEATURE_SECURE_PROCESSING prevents external document access. (XXE/XEE Possible Attacks).
+        fty.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        DocumentBuilder builder = fty.newDocumentBuilder();
+        ByteArrayInputStream bais = new ByteArrayInputStream(xml.getBytes());
+        xmlDoc = builder.parse(bais);
+    }
 
-	public void loadXmlFromBase64(String response) throws ParserConfigurationException, SAXException, IOException {
-		Base64 base64 = new Base64();
-		byte [] decodedB = base64.decode(response);
-		String decodedS = new String(decodedB);
-		loadXml(decodedS);
-	}
+    public void loadXmlFromBase64(String response) throws ParserConfigurationException, SAXException, IOException {
+        Base64 base64 = new Base64();
+        byte[] decodedB = base64.decode(response);
+        String decodedS = new String(decodedB);
+        loadXml(decodedS);
+    }
 
-	public boolean isValid() throws Exception {
-		NodeList nodes = xmlDoc.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
+    // isValid() function should be called to make basic security checks to responses.
+    public boolean isValid() throws Exception {
+        // Security Checks
+        rootElement = xmlDoc.getDocumentElement();
+        Assertions = xmlDoc.getElementsByTagNameNS("urn:oasis:names:tc:SAML:2.0:assertion", "Assertion").getLength();
+        xmlDoc.getDocumentElement().normalize();
 
-		if (nodes == null || nodes.getLength() == 0) {
-			throw new Exception("Can't find signature in document.");
-		}
+        if (Assertions > 1) {
+            throw new Exception("SAML Response must contain 1 Assertion.");
+        }
 
-		if (setIdAttributeExists()) {
-			tagIdAttributes(xmlDoc);
-		}
+        String attName = rootElement.getAttribute("ID");
+        if (attName.equals("")) {
+            throw new Exception("Missing ID attribute on SAML Response.");
+        }
 
-		X509Certificate cert = certificate.getX509Cert();
-		DOMValidateContext ctx = new DOMValidateContext(cert.getPublicKey(), nodes.item(0));
-		XMLSignatureFactory sigF = XMLSignatureFactory.getInstance("DOM");
-		XMLSignature xmlSignature = sigF.unmarshalXMLSignature(ctx);
+        attName = rootElement.getAttribute("Version");
+        if (!attName.equals("2.0")) {
+            throw new Exception("Unsupported SAML Version.");
+        }
 
-		return xmlSignature.validate(ctx);
-	}
+        NodeList nodes = xmlDoc.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
 
-	public String getNameId() throws Exception {
-		NodeList nodes = xmlDoc.getElementsByTagNameNS("urn:oasis:names:tc:SAML:2.0:assertion", "NameID");
+        if (nodes == null || nodes.getLength() == 0) {
+            throw new Exception("Can't find signature in Document.");
+        }
 
-		if(nodes.getLength()==0){
-			throw new Exception("No name id found in document");
-		}
+        if (setIdAttributeExists()) {
+            tagIdAttributes(xmlDoc);
+        }
 
-		return nodes.item(0).getTextContent();
-	}   
-        
-	public String getAttribute(String name) {
-		HashMap attributes = getAttributes();
-		if (!attributes.isEmpty()) {
-			return attributes.get(name).toString();
-		}
-		return null;
-	}
-                        
-	public HashMap getAttributes() {
-		HashMap<String,ArrayList> attributes = new HashMap<>();                   
-		NodeList nodes = xmlDoc.getElementsByTagNameNS("urn:oasis:names:tc:SAML:2.0:assertion", "Attribute");
+        X509Certificate cert = certificate.getX509Cert();
+        DOMValidateContext ctx = new DOMValidateContext(cert.getPublicKey(), nodes.item(0));
+        XMLSignatureFactory sigF = XMLSignatureFactory.getInstance("DOM");
+        XMLSignature xmlSignature = sigF.unmarshalXMLSignature(ctx);
 
-		if(nodes.getLength()!=0){
-			for (int i = 0; i < nodes.getLength(); i++) {
-				NamedNodeMap attrName = nodes.item(i).getAttributes();
-				String attName = attrName.getNamedItem("Name").getNodeValue();
-				NodeList children = nodes.item(i).getChildNodes();
+        return xmlSignature.validate(ctx);
+    }
 
-				ArrayList<String> attrValues = new ArrayList<String>();
-				for (int j = 0; j < children.getLength(); j++) {
-					attrValues.add(children.item(j).getTextContent());
-				}
-				attributes.put(attName, attrValues);
-			}
-		} else {
-		    return null;
-		}
-		return attributes;
-	} 
+    public String getNameId() throws Exception {
+        NodeList nodes = xmlDoc.getElementsByTagNameNS("urn:oasis:names:tc:SAML:2.0:assertion", "NameID");
+        if (nodes.getLength() == 0) {
+            throw new Exception("No name id found in Document.");
+        }
+        return nodes.item(0).getTextContent();
+    }
 
-	private boolean setIdAttributeExists() {
-		for (Method method : Element.class.getDeclaredMethods()) {
-			if (method.getName().equals("setIdAttribute")) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public String getAttribute(String name) {
+        HashMap attributes = getAttributes();
+        if (!attributes.isEmpty()) {
+            return attributes.get(name).toString();
+        }
+        return null;
+    }
 
-	private void tagIdAttributes(Document xmlDoc) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
+    public HashMap getAttributes() {
+        HashMap<String, ArrayList> attributes = new HashMap<>();
+        NodeList nodes = xmlDoc.getElementsByTagNameNS("urn:oasis:names:tc:SAML:2.0:assertion", "Attribute");
+
+        if (nodes.getLength() != 0) {
+            for (int i = 0; i < nodes.getLength(); i++) {
+                NamedNodeMap attrName = nodes.item(i).getAttributes();
+                String attName = attrName.getNamedItem("Name").getNodeValue();
+                NodeList children = nodes.item(i).getChildNodes();
+
+                ArrayList<String> attrValues = new ArrayList<String>();
+                for (int j = 0; j < children.getLength(); j++) {
+                    attrValues.add(children.item(j).getTextContent());
+                }
+                attributes.put(attName, attrValues);
+            }
+        } else {
+            return null;
+        }
+        return attributes;
+    }
+
+    private boolean setIdAttributeExists() {
+        for (Method method : Element.class.getDeclaredMethods()) {
+            if (method.getName().equals("setIdAttribute")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void tagIdAttributes(Document xmlDoc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
-
