ceb5ac817cefdbb40d7d95995f6cb090dfba8bf4
Luis Miranda <lmiranda@atlassian.com>
2016-09-07 11:43:00 +1000

    added framework-independent HttpRequest
    this can be used in java-saml-core where previously we would use
    HttpServletRequest, therefore removing the requirement for clients to be
    running inside a servlet container.

---
+++ core/src/test/java/com/onelogin/saml2/http/HttpRequestTest.java
@@ -0,0 +1,57 @@
+package com.onelogin.saml2.http;
+
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class HttpRequestTest {
+    @Test
+    public void testConstructorWithNoQueryParams() throws Exception {
+        final String url = "url";
+
+        final HttpRequest request = new HttpRequest(url);
+        assertThat(request.getRequestURL(), equalTo(url));
+        assertThat(request.getParameters(), equalTo(Collections.<String, List<String>>emptyMap()));
+        assertThat(request.getParameters("x"), equalTo(Collections.<String>emptyList()));
+        assertThat(request.getParameter("x"), nullValue());
+    }
+
+    @Test
+    public void testConstructorWithQueryParams() throws Exception {
+        final String url = "url";
+        final String name = "name";
+        final String value1 = "val1";
+        final String value2 = "val2";
+
+        final List<String> values = Arrays.asList(value1, value2);
+        final Map<String, List<String>> parametersMap = singletonMap(name, values);
+
+        final HttpRequest request = new HttpRequest(url, parametersMap);
+        assertThat(request.getRequestURL(), equalTo(url));
+        assertThat(request.getParameters(), equalTo(parametersMap));
+        assertThat(request.getParameters(name), equalTo(values));
+        assertThat(request.getParameter(name), equalTo(value1));
+    }
+
+    @Test
+    public void testAddParameter() throws Exception {
+        final String url = "some_url";
+        final String name = "name";
+        final String value = "value";
+
+        final HttpRequest request = new HttpRequest(url).addParameter(name, value);
+        assertThat(request.getRequestURL(), equalTo(url));
+        assertThat(request.getParameters(), equalTo(singletonMap(name, singletonList(value))));
+        assertThat(request.getParameters(name), equalTo(singletonList(value)));
+        assertThat(request.getParameter(name), equalTo(value));
+    }
+}
