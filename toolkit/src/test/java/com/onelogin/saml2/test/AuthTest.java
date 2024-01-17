e0a996cc3e982ae2680552716bd961e73bc10c99
Oleh Berest <oleh.berest@borm.ch>
2024-01-17 11:10:53 +0100

    Migration to jakarta packages

--- toolkit/src/test/java/com/onelogin/saml2/test/AuthTest.java
+++ toolkit/src/test/java/com/onelogin/saml2/test/AuthTest.java
@@ -37,9 +37,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.junit.Rule;
 import org.junit.Test;
--- toolkit/src/test/java/com/onelogin/saml2/test/servlet/ServletUtilsTest.java
+++ toolkit/src/test/java/com/onelogin/saml2/test/servlet/ServletUtilsTest.java
@@ -13,8 +13,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
 
